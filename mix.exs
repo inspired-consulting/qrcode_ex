2e3204b47432033d81fed24d96efbaa96c669d72
Ruben Wagner <ruben.wagner@globus.ch>
2022-01-14 20:08:35 +0100

    raise version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule QRCodeEx.MixProject do
   def project do
     [
       app: :qrcode_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "QRCodeEx",
